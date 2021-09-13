97f0459bdaed33f50a3150966b50fe79a6e5df25
hefenyuan <102140847@qq.com>
2021-09-13 17:34:19 +0800

    Update OracleConst.java
    oralce修改语句字段不能有双引号 \"

--- src/main/java/net/lab1024/smartdb/sqlbuilder/impl/oracle/OracleConst.java
+++ src/main/java/net/lab1024/smartdb/sqlbuilder/impl/oracle/OracleConst.java
@@ -4,5 +4,5 @@ package net.lab1024.smartdb.sqlbuilder.impl.oracle;
  * oracle
  */
 public class OracleConst {
-    static final String RESERVED_WORD_CHAR = "\"";
+    static final String RESERVED_WORD_CHAR = "";
 }
